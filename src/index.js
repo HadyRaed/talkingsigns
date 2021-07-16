bccf16be5f328ad01de9a42a5648d9f793a33cb1
hadyraed <hraed666@gmail.com>
2021-07-16 14:27:26 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 
 app.use(photoRouter)
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 
 app.listen(port, () => {
     console.log(`server is up on port ${port}`);
